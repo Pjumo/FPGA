021f504d12e4f9aca752b98b9e6e3cd60218fa30
Pjumo <parkjunmozz55@gmail.com>
2026-03-04 11:28:21 +0900

    03/04

--- 14.rotary_encoder/14.rotary_encoder.sim/sim_1/behav/xsim/xsim.dir/tb_top_rotary_behav/webtalk/xsim_webtalk.tcl
+++ 14.rotary_encoder/14.rotary_encoder.sim/sim_1/behav/xsim/xsim.dir/tb_top_rotary_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir G:/Project/FPGA/ai1/14.rotary_encoder/14.rotary_encoder.sim/sim_1/behav/xsim/xsim.dir/tb_top_rotary_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Wed Mar  4 09:56:44 2026" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Wed Mar  4 11:18:19 2026" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2021.1 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3247384" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "23b55981-a982-4af8-957b-c1e6b4d50b7b" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "08e1846afad44fb3ae7a1fedc5520bdb" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "9" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "12" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-9700 CPU @ 3.00GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "30950 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "10032_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3146029866 -regid "" -xml G:/Project/FPGA/ai1/14.rotary_encoder/14.rotary_encoder.sim/sim_1/behav/xsim/xsim.dir/tb_top_rotary_behav/webtalk/usage_statistics_ext_xsim.xml -html G:/Project/FPGA/ai1/14.rotary_encoder/14.rotary_encoder.sim/sim_1/behav/xsim/xsim.dir/tb_top_rotary_behav/webtalk/usage_statistics_ext_xsim.html -wdm G:/Project/FPGA/ai1/14.rotary_encoder/14.rotary_encoder.sim/sim_1/behav/xsim/xsim.dir/tb_top_rotary_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.06_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "10132_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 1217494565 -regid "" -xml G:/Project/FPGA/ai1/14.rotary_encoder/14.rotary_encoder.sim/sim_1/behav/xsim/xsim.dir/tb_top_rotary_behav/webtalk/usage_statistics_ext_xsim.xml -html G:/Project/FPGA/ai1/14.rotary_encoder/14.rotary_encoder.sim/sim_1/behav/xsim/xsim.dir/tb_top_rotary_behav/webtalk/usage_statistics_ext_xsim.html -wdm G:/Project/FPGA/ai1/14.rotary_encoder/14.rotary_encoder.sim/sim_1/behav/xsim/xsim.dir/tb_top_rotary_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
--- 14.rotary_encoder/14.rotary_encoder.srcs/sources_1/new/rotary.v
+++ 14.rotary_encoder/14.rotary_encoder.srcs/sources_1/new/rotary.v
@@ -12,28 +12,32 @@ module rotary(
     reg [1:0] r_prev_state = 2'b00;
     reg [1:0] r_current_state = 2'b00;
     reg [7:0] r_counter = 8'h00;
+    reg [1:0] r_counter_2b = 2'b00;
 
     // s1, s2
     always @(posedge clk, posedge reset) begin
         if(reset) begin
-            r_direction = 2'b00;
-            r_prev_state = 2'b00;
-            r_current_state = 2'b00;
-            r_counter = 8'h00;
+            r_direction <= 2'b00;
+            r_prev_state <= 2'b00;
+            r_current_state <= 2'b00;
+            r_counter <= 8'h00;
+            r_counter_2b <= 2'b00;
         end else begin  // 동시성을 고려했을 때, 다음 clk에서 r_prev, r_current 적용됨
             r_prev_state <= r_current_state;
             r_current_state <= {clean_s1, clean_s2};
             case({r_prev_state, r_current_state})
                 4'b0010, 4'b1011, 4'b1101, 4'b0100: begin
-                    if(r_counter < 8'hFF)   // overflow
+                    if(r_counter < 8'hFF && r_counter_2b == 2'b11)   // overflow
                         r_counter <= r_counter + 1;
                     
+                    r_counter_2b <= r_counter_2b + 1;
                     r_direction <= 2'b01;
                 end
                 4'b0001, 4'b0111, 4'b1110, 4'b1000: begin
-                    if(r_counter > 8'h00)
+                    if(r_counter > 8'h00 && r_counter_2b == 2'b11)
                         r_counter <= r_counter - 1;
                     
+                    r_counter_2b <= r_counter_2b + 1;
                     r_direction <= 2'b10;
                 end
             endcase
